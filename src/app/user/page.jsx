5ad57bf8b007838389e5412096d10449d2204c94
ahmed0samy <86538090+ahmed0samy@users.noreply.github.com>
2025-07-08 12:38:52 +0300

    Your update message

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -1,22 +1,36 @@
-"use client"
-import { useEffect } from 'react'
-import Peer from 'simple-peer'
-import { socket } from '@/lib/socket'
+"use client";
+import { useEffect } from "react";
+import Peer from "simple-peer";
+import { socket } from "@/lib/socket";
 
 export default function User() {
   useEffect(() => {
-    navigator.mediaDevices.getDisplayMedia({ video: true }).then(stream => {
-      const peer = new Peer({ initiator: true, trickle: false, stream })
+    navigator.mediaDevices.getDisplayMedia({ video: true }).then((stream) => {
+      const peer = new Peer({ initiator: true, trickle: false, stream });
 
-      peer.on('signal', signal => {
-        socket.emit('user-ready', signal)
-      })
+      peer.on("signal", (signal) => {
+        socket.emit("user-ready", signal);
+      });
 
-      socket.on('admin-accepted', answer => {
-        peer.signal(answer)
-      })
-    })
-  }, [])
+      socket.on("admin-accepted", (answer) => {
+        peer.signal(answer);
+      });
+    });
+  }, []);
 
-  return <p>Screen sharing started...</p>
+  return (
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLScYWRK4nplfd9NeXcr6ELkMea5FYwg83XHANGPRyWLBNS_UiA/viewform?embedded=true"
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        border: "none",
+      }}
+    >
+      جارٍ التحميل…
+    </iframe>
+  );
 }
